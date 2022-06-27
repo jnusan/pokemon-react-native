85cf68e5e880835e600f12ae43b88b15b755a2ed
jnusan <joseenrique0595@gmail.com>
2022-06-26 21:53:04 -0600

    Adding pokemon1 and pokemon2 to Pokemon context to create attack feature

--- src/context/PokemonContext.tsx
+++ src/context/PokemonContext.tsx
@@ -6,12 +6,18 @@ const PokemonContext = React.createContext({});
 const PokemonProvider = ({ children }) => {
   const [pokemon1, setPokemon1] = useState<IPokemon>();
   const [pokemon2, setPokemon2] = useState<IPokemon>();
+  const [pokemon1Data, setPokemon1Data] = useState();
+  const [pokemon2Data, setPokemon2Data] = useState();
   return (
     <PokemonContext.Provider value={{ 
       pokemon1, 
       setPokemon1,
       pokemon2, 
-      setPokemon2
+      setPokemon2,
+      pokemon1Data,
+      setPokemon1Data,
+      pokemon2Data,
+      setPokemon2Data
      }}>
       { children }
     </PokemonContext.Provider>
