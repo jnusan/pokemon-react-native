6fbc6664825321e66e13288ea71bce21cb74b283
jnusan <joseenrique0595@gmail.com>
2022-06-26 21:52:35 -0600

    Updating limit to 150

--- src/hooks/usePokemon.ts
+++ src/hooks/usePokemon.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { reqResPokemonApi } from "../api/reqResPokemon";
 import { IReqRes, IPokemon } from "../interfaces/reqResPokemon";
 
-const LIMIT = 10;
+const LIMIT = 150;
 const OFF_SET = 0;
 
 export const usePokemon = () => {
@@ -23,9 +23,6 @@ export const usePokemon = () => {
 
     if(resp.data.results.length > 0) {
       setPokemon(resp.data.results);
-    } else {
-      // TODO: Add here flash message to inform the user if exists a issue getting the information from API
-      // alert('Something happened on getting pokemons');
     }
   }
 
