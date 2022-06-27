0b878448d37bbaeec65c5c639d0027986e0144fa
jnusan <joseenrique0595@gmail.com>
2022-06-26 21:54:12 -0600

    Updating pokemon1 and pokemon2 data from context to get information on other components

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -1,17 +1,17 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { Button, Text, View } from 'react-native';
+import { Button, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
 import {Picker} from '@react-native-picker/picker';
 
 import { usePokemon } from '../hooks/usePokemon';
 import { IPokemon } from '../interfaces/reqResPokemon';
 import { PokemonContext } from '../context/PokemonContext'
 import { SelectPokemon } from './SelectPokemon';
+import { ShowPokemon } from './ShowPokemon';
 import axios from 'axios';
 
 export const Pokemon = () => {
-  const { pokemon1, pokemon2 } = useContext(PokemonContext);
-  const [ pokemon1Result, setPokemon1Result ] = useState();
-  const [ pokemon2Result, setPokemon2Result ] = useState();
+  const { pokemon1, pokemon2,  pokemon1Data, setPokemon1Data, pokemon2Data, setPokemon2Data } = useContext(PokemonContext);
+  const [ loadingPokemonInfo, setLoadingPokemonInfo ] = useState();
 
   const startBattle = async () => {
     if(pokemon1 && pokemon2) {
@@ -19,42 +19,50 @@ export const Pokemon = () => {
     }
   }
 
+  const getPokemonData = (data) => {
+    return {
+      hp: data.stats[0].base_stat,
+      sprites: data.sprites,
+      name: data.name,
+      moves: data.moves,
+      max: data.stats[0].base_stat
+    }
+  }
+
   const getPokemonInfo = async () => {
+    setLoadingPokemonInfo(true);
     const resp1 = await axios.get(pokemon1.url);
     const resp2 = await axios.get(pokemon2.url);
-    console.log(resp1.data);
-    console.log(resp2.data);
-    setPokemon1Result(resp1.data);
-    setPokemon2Result(resp2.data);
+    setPokemon1Data( getPokemonData(resp1.data));
+    setPokemon2Data(getPokemonData(resp2.data));
+    setLoadingPokemonInfo(false);
   }
 
   return (
-    <>
-      <View>
-        <SelectPokemon pokemonPos={1}/>
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          <SelectPokemon pokemonPos={1}/>
+          <SelectPokemon pokemonPos={2}/>
+          <Button
+            onPress={ startBattle }
+            title={loadingPokemonInfo ? 'Loading pokemon...' : 'Fight!'}
+            color="#841584"
+            disabled={loadingPokemonInfo ? true : false}
+          />
 
-        {
-          pokemon1Result && 
-          <Text>
-            Pokemon 1 with info
-          </Text>
-        }
-
-        <SelectPokemon pokemonPos={2}/>
-
-        {
-          pokemon2Result && 
-          <Text>
-            Pokemon 2 with info
-          </Text>
-        }
-
-        <Button
-          onPress={ startBattle }
-          title="Fight!"
-          color="#841584"
-        />
-      </View>
-    </>
+          <View>
+            {
+              pokemon2Data && 
+              <ShowPokemon pokemon={pokemon2Data} position={2}/>
+            }
+            {
+              pokemon1Data && 
+              <ShowPokemon pokemon={pokemon1Data} position={1} />
+            }
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
